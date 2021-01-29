fcf66f1548db263243b36a00d3922c5bb5f093fa
zzz <281682188@qq.com>
2021-01-29 18:01:14 +0800

    add list from lambda

---
+++ List_test.go
@@ -0,0 +1,17 @@
+package lambda
+
+import (
+	"testing"
+)
+
+func Test__obj_IndexOf(t *testing.T) {
+	t.Fail()
+}
+
+func Test__obj_Insert(t *testing.T) {
+	t.Fail()
+}
+
+func Test__obj_RemoveAt(t *testing.T) {
+	t.Fail()
+}
